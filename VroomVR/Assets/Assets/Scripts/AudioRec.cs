391c1d5d70c028852b917f2a70bb46765d8fbbec
Shannon <shannonke@gatech.edu>
2018-10-20 13:50:51 -0400

    added recording features

---
+++ VroomVR/Assets/Assets/Scripts/AudioRec.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRec : MonoBehaviour {
+	AudioClip myAudioClip; 
+ 
+     void Start() {}
+     void Update () {}
+     
+    void OnGUI()
+    {
+         if (GUI.Button(new Rect(10,10,60,50),"Record"))
+     { 
+         myAudioClip = Microphone.Start ( null, false, 10, 44100 );
+     }
+     if (GUI.Button(new Rect(10,70,60,50),"Save"))
+     {
+         SavWav.Save("audio", myAudioClip);
+ 
+ //        audio.Play();
+         }
+    }
+}
---
+++ VroomVR/Assets/Assets/Scripts/QuickStart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.TextToSpeech.V1;
+
+public class QuickStart
+{
+    public static void Main(string[] args)
+    {
+        // Instantiate a client
+        TextToSpeechClient client = TextToSpeechClient.Create();
+
+        // Set the text input to be synthesized.
+        SynthesisInput input = new SynthesisInput
+        {
+        		///clever bot return stuff
+            Text = "Hello, World!"
+        };
+
+        // Build the voice request, select the language code ("en-US"),
+        // and the SSML voice gender ("neutral").
+        VoiceSelectionParams voice = new VoiceSelectionParams
+        {
+            LanguageCode = "en-US",
+            SsmlGender = SsmlVoiceGender.Neutral
+        };
+
+        // Select the type of audio file you want returned.
+        AudioConfig config = new AudioConfig
+        {
+            AudioEncoding = AudioEncoding.Mp3
+        };
+
+        // Perform the Text-to-Speech request, passing the text input
+        // with the selected voice parameters and audio file type
+        var response = client.SynthesizeSpeech(new SynthesizeSpeechRequest
+        {
+            Input = input,
+            Voice = voice,
+            AudioConfig = config
+        });
+
+        // Write the binary AudioContent of the response to an MP3 file.
+        using (Stream output = File.Create("sample.mp3"))
+        {
+            response.AudioContent.WriteTo(output);
+            Debug.Log("Audio content written to file 'sample.mp3'");
+        }
+    }
+}
